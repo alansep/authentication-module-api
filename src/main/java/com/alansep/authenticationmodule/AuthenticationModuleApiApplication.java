e362fd7fefa1300c59db7090e9d8ee100b873201
Gabriel Alan <gabrielalan.development@outlook.com>
2022-12-24 15:06:18 -0300

    Dependencies have been added.

---
+++ src/main/java/com/alansep/authenticationmodule/AuthenticationModuleApiApplication.java
@@ -0,0 +1,13 @@
+package com.alansep.authenticationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationModuleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationModuleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alansep/authenticationmodule/AuthenticationModuleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alansep.authenticationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationModuleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
