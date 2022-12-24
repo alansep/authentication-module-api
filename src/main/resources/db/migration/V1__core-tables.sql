fe84ddc04bea3d17635ddb4715eb01d085e66b8a
Gabriel Alan <gabrielalan.development@outlook.com>
2022-12-24 16:12:47 -0300

    Spring Data JPA has been configured and core migration has been written.

---
+++ src/main/resources/db/migration/V1__core-tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `roles` (
+  `id` varchar(36) PRIMARY KEY,
+  `name` varchar(50) NOT NULL
+);
+
+CREATE TABLE `users` (
+	`id` varchar(36) PRIMARY KEY,
+	`username` varchar(20) NOT NULL,
+	`password` varchar(50) NOT NULL
+);
+
+CREATE TABLE `user_roles` (
+	`id` varchar(36) PRIMARY KEY,
+	`user_id` varchar(36) NOT NULL,
+	`role_id` varchar(36) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (role_id) REFERENCES roles(id));
